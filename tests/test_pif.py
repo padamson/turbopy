cd99e91101e9bff0b4929f7c96990edd00f9d3cb
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-07-21 16:56:41 -0400

    first working version of integration test that compares output files to good versions of files

--- tests/test_pif.py
+++ tests/test_pif.py
@@ -1,5 +1,13 @@
+"""Integration test based on particle_in_field turbopy app
+
+This integration test simply runs the particle_in_field app and compares the output files
+to "good" output.
+"""
 from abc import ABCMeta
 from turbopy.core import PhysicsModule
+import io
 
 def test_pif(pif_run):
-    assert isinstance(PhysicsModule, ABCMeta)
+    for filename in ['e_0.5', 'grid', 'particle_p', 'particle_x', 'time']:
+        assert (list(io.open(f'tests/fixtures/particle_in_field/output/{filename}.csv')) ==
+                list(io.open(f'tmp/particle_in_field/output/{filename}.csv')))
